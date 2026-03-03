d006e4862cf74013b1467dfdfac0a76e7d2dc49c
VivekVerma12 <vermavivek8084@gmail.com>
2026-03-03 22:08:35 +0530

    Added expeirence page.

---
+++ src/components/ExperienceForm.jsx
@@ -0,0 +1,48 @@
+import { Plus } from 'lucide-react'
+import React from 'react'
+
+const ExperienceForm = ({data , onChange}) => {
+
+    const addExperience = ()=>{
+        const newExperience = {
+            company: "",
+            position: "",
+            start_date: "",
+            end_date: "",
+            description: "",
+            is_current: false
+        };
+        onChange([...data, newExperience])
+        
+    }
+
+    const removeExperience = (index) => {
+        const updated = data.filter((_, i) => i !== index);
+        onChange(updated);
+    }
+
+     const updatedExperience = (index,field,value) => {
+        const updated = [...data];
+         updated[index] = {...updated[index]} = data.filter((_, i) => i !== index);
+        onChange(updated);
+    }
+
+  return (
+    <div className='space-y-6'>
+         <div className='flex items-center justify-between'>
+                <div>
+                    <h3 className='flex items-center gap-2 text-lg font-semibold text-gray-900'>
+                        Professional Experience
+                    </h3>
+                    <p className='text-sm text-gray-500'>Add your job experience here</p>
+                </div>
+                <button className='flex items-center gap-2 px-3 py-1 text-sm bg-purple-100
+                text-purple-700 rounded hover:bg-purple-200 transition-colors'>
+                    <Plus className='size-4' /> Add Experience
+                </button>
+            </div>
+    </div>
+  )
+}
+
+export default ExperienceForm
